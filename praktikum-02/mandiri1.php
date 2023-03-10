b1521b88f5fae0f31cc9f1f3ab8dc701f04cf329
Shofi Syahria Mahram <shofishifa31@gmail.com>
2023-03-10 16:00:30 +0700

    commit

--- praktikum-02/mandiri1.php
+++ praktikum-02/mandiri1.php
@@ -11,11 +11,11 @@
 
     <title>Form Belanja</title>
 </head>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
 <body>
@@ -59,35 +59,35 @@
                 </div>
             </form>
         </div>
-            <div class="card" >
-                <h5 class="card-header" style="background-color: #F0FFFF; color:black;">
-                    Daftar Barang dan Harga
-                </h5>
-                <div class="card-body">
-                    <p class="card-text">
-                        TV : 4.200.000
-                    </p>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        Kulkas : 3.100.000
-                    </p>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        Mesin Cuci : 3.800.000
-                    </p>
-                </div>
-                <div class="card-footer" style="background-color: #F0FFFF; color:black; ">
-                    Harga Dapat Saja Berubah.
-                </div>
+        <div class="card">
+            <h5 class="card-header" style="background-color: #F0FFFF; color:black;">
+                Daftar Barang dan Harga
+            </h5>
+            <div class="card-body">
+                <p class="card-text">
+                    TV : 4.200.000
+                </p>
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    Kulkas : 3.100.000
+                </p>
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    Mesin Cuci : 3.800.000
+                </p>
+            </div>
+            <div class="card-footer" style="background-color: #F0FFFF; color:black; ">
+                Harga Dapat Saja Berubah.
             </div>
         </div>
+    </div>
     </div>
     <br>
     <hr color="black"><br>
     <div class="row">
-        <div class="col-md-3" >
+        <div class="col-md-3">
             <?php
 
             $proses = $_POST["proses"];
--- praktikum-02/mandiri2.php
+++ praktikum-02/mandiri2.php
@@ -1,77 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mandiri 2</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mandiri 2</title>
 </head>
 
 <body>
-<form method="POST">
-  Nama : <input type="text" name="nama" value="" size="30"></br>
-  Nilai UTS : <input type="text" name="nilai_uts" value="" size="6"><br>
-  Nilai UAS : <input type="text" name="nilai_uas" value="" size="6"><br>
-  Nilai Tugas : <input type="text" name="nilai_tugas" value="" size="6"><br>
-  <button name="proses">Simpan</button>
-</form>
+  <form method="POST">
+    Nama : <input type="text" name="nama" value="" size="30"></br>
+    Nilai UTS : <input type="text" name="nilai_uts" value="" size="6"><br>
+    Nilai UAS : <input type="text" name="nilai_uas" value="" size="6"><br>
+    Nilai Tugas : <input type="text" name="nilai_tugas" value="" size="6"><br>
+    <button name="proses">Simpan</button>
+  </form>
 
-<?php
-$nama = $_POST ['nama'];
-$nilai_uts = $_POST ['nilai_uts'];
-$nilai_uas = $_POST ['nilai_uas'];
-$nilai_tugas = $_POST ['nilai_tugas'];
-$tombol = $_POST ['proses'];
+  <?php
+  $nama = $_POST['nama'];
+  $nilai_uts = $_POST['nilai_uts'];
+  $nilai_uas = $_POST['nilai_uas'];
+  $nilai_tugas = $_POST['nilai_tugas'];
+  $tombol = $_POST['proses'];
 
-// variabel total nilai
-$total_nilai = $nilai_uts*30/100 + $nilai_uas*35/100 + $nilai_tugas*35/100;
+  // variabel total nilai
+  $total_nilai = $nilai_uts * 30 / 100 + $nilai_uas * 35 / 100 + $nilai_tugas * 35 / 100;
 
-//Logic Status
-$status;
-if ($total_nilai >= 55) { 
-  $status = "Lulus";
-} 
-else {
-  $status = "Tidak Lulus";
-}
+  //Logic Status
+  $status;
+  if ($total_nilai >= 55) {
+    $status = "Lulus";
+  } else {
+    $status = "Tidak Lulus";
+  }
 
-// logic grade
-if($total_nilai >= 85 && $total_nilai <= 100) $grade = "A";
-elseif ($total_nilai >= 70 && $total_nilai <= 84) $grade = "B";
-elseif ($total_nilai >= 56 && $total_nilai <= 69) $grade = "C";
-elseif ($total_nilai >= 36 && $total_nilai <= 55) $grade = "D";
-elseif ($total_nilai >= 0 && $total_nilai <= 35) $grade = "E";
-else $grade = "";
+  // logic grade
+  if ($total_nilai >= 85 && $total_nilai <= 100) $grade = "A";
+  elseif ($total_nilai >= 70 && $total_nilai <= 84) $grade = "B";
+  elseif ($total_nilai >= 56 && $total_nilai <= 69) $grade = "C";
+  elseif ($total_nilai >= 36 && $total_nilai <= 55) $grade = "D";
+  elseif ($total_nilai >= 0 && $total_nilai <= 35) $grade = "E";
+  else $grade = "";
 
-//logic predikat
- switch ($grade) {
-  case 'A': $predikat = 'Sangat Baik' ; break;
-  case 'B': $predikat = 'Baik' ; break;
-  case 'C': $predikat = 'Cukup' ; break;
-  case 'D': $predikat = 'Kurang Baik' ; break;
-  case 'E': $predikat = 'Tidak Baik' ; break;
-  
-  default: $predikat = '' ;
- }
- if(isset($tombol)) { ?>
+  //logic predikat
+  switch ($grade) {
+    case 'A':
+      $predikat = 'Sangat Baik';
+      break;
+    case 'B':
+      $predikat = 'Baik';
+      break;
+    case 'C':
+      $predikat = 'Cukup';
+      break;
+    case 'D':
+      $predikat = 'Kurang Baik';
+      break;
+    case 'E':
+      $predikat = 'Tidak Baik';
+      break;
 
- Nama Siswa : <?= $nama ?>
- <br>
- Nilai UTS : <?= $nilai_uts ?>
- <br>
- Nilai UAS : <?= $nilai_uas ?>
- <br>
- Nilai Tugas : <?= $nilai_tugas ?>
- <br>
- Total Nilai : <?= $total_nilai ?>
- <br>
- Keterangan : <?= $status ?>
- <br>
- Grade : <?= $grade ?>
- <br>
- Predikat : <?= $predikat ?>
-<?php } ?>
+    default:
+      $predikat = '';
+  }
+  if (isset($tombol)) { ?>
+
+    Nama Siswa : <?= $nama ?>
+    <br>
+    Nilai UTS : <?= $nilai_uts ?>
+    <br>
+    Nilai UAS : <?= $nilai_uas ?>
+    <br>
+    Nilai Tugas : <?= $nilai_tugas ?>
+    <br>
+    Total Nilai : <?= $total_nilai ?>
+    <br>
+    Keterangan : <?= $status ?>
+    <br>
+    Grade : <?= $grade ?>
+    <br>
+    Predikat : <?= $predikat ?>
+  <?php } ?>
 
 
 </body>
+
 </html>
